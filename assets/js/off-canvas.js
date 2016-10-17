8f74a3cf688673dd8d7b34ac1ae259d6b6fd4df7
Preston Edmands <pedmands@gmail.com>
2016-10-17 18:33:22 -0400

    Added off-canvas fixed menu

--- assets/js/off-canvas.js
+++ assets/js/off-canvas.js
@@ -1,26 +1,12 @@
-/*
-NOW AS A SUPER EASY TO USE PLUGIN:
-http://panels.scotch.io
-*/
-$(function() {
-  var special = ['reveal', 'top', 'boring', 'perspective', 'extra-pop'];
+$(document).ready(function() {
 
   // Toggle Nav on Click
-  $('#menu-toggle').click(function() {
-
-    var transitionClass = $(this).data('transition');
-
-    if ($.inArray(transitionClass, special) > -1) {
-      $('body').removeClass();
-      $('body').addClass(transitionClass);
-    } else {
-      $('body').removeClass();
-      $('#site-canvas').removeClass();
-      $('#site-canvas').addClass(transitionClass);
-    }
-
-    $('#site-wrapper').toggleClass('show-nav');
-    $('.site-content').toggleClass('faded');
+  $('button').click(function() {
+      var transition = $.parseJSON($(this).attr('data-button'));
+      $('#content').addClass(transition);
+      $('#site-wrapper').toggleClass('show-nav');
+      $('#menu').toggleClass('open');
+      $('#content').toggleClass('faded');
     return false;
 
   });
